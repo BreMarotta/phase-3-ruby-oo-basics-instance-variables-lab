192c80662d789b55112fc050fa8feb2c5c2b8111
Breanne <BreMarotta@gmail.com>
2022-03-06 17:49:34 -0500

    commit tree 224b751765dadbe0a5d8d74be935cbccf4cf698c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,34 @@
 class Dog
 
-end
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  
+  puts lassie.name
+
+# class Dog
+#     # setter method
+#     def name=(value)
+#         # assign the @name instance variable a value of whatever is passed as an argument
+#         @name = value
+#     end
+
+#     # getter method
+#     def name
+#         # access the data fromt he @name instance variabel and return it
+#         @name
+#     end
+# end
+
+# odie = Dog.new 
+# odie.name = "Odie"
+
+# puts odie.name
